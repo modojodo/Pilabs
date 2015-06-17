66ed55279d179ee0ab0013adf01f69f28661f31e
Umer Hassan <umerhassan94@gmail.com>
2015-06-17 12:20:19 +0500

    All projects along with readme

---
+++ Chat App/js/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Umer on 6/16/2015.
+ */
+var networkFunc = new Network();
+var msgObj = new Message();
+var URL = "http://datastore.asadmemon.com/chat/";
+var nick = "";
+var timeStamp = new Date().getTime();
+var pointer = 0;
+var finalMessage = {};
+$(function () {
+
+    $("#nickDone").on("click", function () {
+        if ($("#chooseNickTxt").val() == "") {
+            alert("Choose a valid nick!");
+        } else {
+            nick = $("#chooseNickTxt").val();
+        }
+        $("#nickChoice").hide();
+        $("#userNick").show();
+        $("#finalNick").text(nick);
+    });
+
+
+    msgObj.sendMessage();
+    msgObj.recieveMessage();
+
+
+});
